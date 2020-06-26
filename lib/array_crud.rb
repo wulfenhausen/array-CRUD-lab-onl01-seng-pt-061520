57b570dd473483114516322ffd083bc5411f3c58
William Han Findiesen <william.findiesen@gmail.com>
2020-06-26 04:15:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("am")
+  array.unshift("wow")
 
 end
 
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[0]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
