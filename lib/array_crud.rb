599c1f4ade40dfb2050a6530962eb8b93cc5b1fc
William Han Findiesen <william.findiesen@gmail.com>
2020-06-26 02:34:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
   end
 
 def add_element_to_end_of_array
-  array << "arrays!"
+  #3333333333array << "arrays!"
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array
