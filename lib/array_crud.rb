308f722b1f50d174907994ea2ef3cc981526a117
William Han Findiesen <william.findiesen@gmail.com>
2020-06-26 04:11:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,12 +26,11 @@ def remove_element_from_start_of_array(array)
   array.shift 
 end
 
-def retrieve_element_from_index(array, element)
+def retrieve_element_from_index(array, index_number)
   array[0]
 end
 
 def retrieve_first_element_from_array
-  array = ["1","2","3","4","5"]
   return array[0]
 end
 
