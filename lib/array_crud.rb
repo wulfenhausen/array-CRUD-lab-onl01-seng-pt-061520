de9fa52a6eb2cdd3f61625d1169ceaf3eec9aae9
William Han Findiesen <william.findiesen@gmail.com>
2020-06-26 02:31:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,13 +3,13 @@ def create_an_empty_array
 end
 
 def create_an_array
-  #Array.new(4)
+  # or Array.new(4)
   array = [1,2,3,4]
   end
 
 def add_element_to_end_of_array
-  array = ["1","2","3"]
-  array.push("4")
+  array = [1,2,3]
+  array.push(4)
 end
 
 def add_element_to_start_of_array
